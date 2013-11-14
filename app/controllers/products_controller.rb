35cd14708323780c1d12485c15b5e7da776e0b56
India Meisner <india.rae@gmail.com>
2013-11-13 21:15:50 -0800

    Update products controller to use strong params

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,11 @@
 class ProductsController < ApplicationController
 
 	def create
-		@product = current_user.products.new(params[:product])
+		@product = current_user.products.new(product_params)
 		if @product.save
 		redirect_to user_url(current_user), :notice => "#{@product.name}s listed!"
 		else
-		  redirect_to user_url(current_user), :alert => "Purchase not listed!"
+		  redirect_to user_url(current_user), :alert => "Product not listed!"
 		end
 	end
 
@@ -16,5 +16,10 @@ class ProductsController < ApplicationController
 		@product = Product.find(params[:id])
 		@product.destroy
 		redirect_to user_url(current_user)
+	end
+
+	private
+	def product_params
+		params.require(:product).permit(:model, :brand, :amount)
 	end
 end
