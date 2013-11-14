c424e2b74f227b66fb5762ab4b6f9e5d16c2c467
India Meisner <india.rae@gmail.com>
2013-11-13 17:12:44 -0800

    Initial commit. Updated Gemfile and installed simple_form_for and rails_footnotes

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ShoeSales::Application.config.secret_key_base = '2d284f3f75d34556b387b4df1cf8b4f3d11be007b979d22c0a20b090c2347771f3aaec36c711c8b0008f908c12509d60262cf4c0f75addcf9f59a93b00bc305b'
