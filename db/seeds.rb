4e8f46e35e2ebe9b84dd57b869062db0f2e7d08b
India Meisner <india.rae@gmail.com>
2013-11-13 19:28:39 -0800

    Add test users to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(first_name:"Muffin", last_name:"Kitty", email:"muffin@kitty.com", password:"qqqqqqq", password_confirmation:"qqqqqqq")
+
+User.create(first_name:"Biscuit", last_name:"Kitty", email:"biscuit@kitty.com", password:"qqqqqqq", password_confirmation:"qqqqqqq")
+
+User.create(first_name:"Space", last_name:"Cowboy", email:"maurice@spacecowboy.com", password:"gangsteroflove", password_confirmation:"gangsteroflove")
+
+User.create(first_name:"Jay", last_name:"Gatsby", email:"jay@gatsby.com", password:"qqqqqqq", password_confirmation:"qqqqqqq")
+
+User.create(first_name: "Daisy", last_name: "Buchanan", email: "daisy@buchanan.com", password:"qqqqqqq", password_confirmation:"qqqqqqq")
+
+User.create(first_name: "Nick",	last_name: "Carraway", email: "nick@carraway.com", password:"qqqqqqq", password_confirmation:"qqqqqqq")
+
+User.create(first_name: "Tom", last_name: "Buchanan", email: "tom@buchanan.com", password:"qqqqqqq", password_confirmation:"qqqqqqq")
+
+User.create(first_name: "Jordan", last_name: "Baker", email: "jordan@baker.com", password:"qqqqqqq", password_confirmation:"qqqqqqq")
